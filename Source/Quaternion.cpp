37ca12dc77d5d1e123412fd09c7dc95bb1d0f5c7
Quas NaArt <93419246+Quas-NaArt@users.noreply.github.com>
2025-08-13 20:46:31 -0500

    preparing quaternions for constexpr and nodiscard

--- Source/Quaternion.cpp
+++ Source/Quaternion.cpp
@@ -1,63 +1,32 @@
 #include "Quaternion.h"
-#define _USE_MATH_DEFINES // For M_PI_4
-#include <math.h>
-#include <cmath>
-
-Quaternion::Quaternion(float X, float Y, float Z, float W)
-{
-    x = X; y = Y; z = Z; w = W;
-}
-
-Quaternion::Quaternion(float pitch, float yaw, float roll)
-{
-    pitch *= M_PI / 180.0f; yaw *= M_PI / 180.0f; roll *= M_PI / 180.0f;
-    x = std::sin(roll / 2) * std::cos(pitch / 2) * std::cos(yaw / 2) - std::cos(roll / 2) * std::sin(pitch / 2) * std::sin(yaw / 2);
-    y = std::cos(roll / 2) * std::sin(pitch / 2) * std::cos(yaw / 2) + std::sin(roll / 2) * std::cos(pitch / 2) * std::sin(yaw / 2);
-    z = std::cos(roll / 2) * std::cos(pitch / 2) * std::sin(yaw / 2) - std::sin(roll / 2) * std::sin(pitch / 2) * std::cos(yaw / 2);
-    w = std::cos(roll / 2) * std::cos(pitch / 2) * std::cos(yaw / 2) + std::sin(roll / 2) * std::sin(pitch / 2) * std::sin(yaw / 2);
-}
-
-double Quaternion::Length() const {
-    return std::sqrt(w * w + x * x + y * y + z * z);
-}
-
-Quaternion Quaternion::Normalize() {
-    float length = (float)Length();
-    w /= length;
-    x /= length;
-    y /= length;
-    z /= length;
-    return *this;
-}
-
-Quaternion Quaternion::Mul(const Quaternion& other) const {
-    return Quaternion{
-        w * other.w - x * other.x - y * other.y - z * other.z,
-        w * other.x + x * other.w + y * other.z - z * other.y,
-        w * other.y - x * other.z + y * other.w + z * other.x,
-        w * other.z + x * other.y - y * other.x + z * other.w
-    };
-}
-
-void Quaternion::RotateVector(std::vector<float>& v)
-{
-    std::vector<float> vector;
-    float num12 = x + x;
-    float num2 = y + y;
-    float num = z + z;
-    float num11 = w * num12;
-    float num10 = w * num2;
-    float num9 = w * num;
-    float num8 = x * num12;
-    float num7 = x * num2;
-    float num6 = x * num;
-    float num5 = y * num2;
-    float num4 = y * num;
-    float num3 = z * num;
-    float num15 = ((v[0] * ((1.0f - num5) - num3)) + (v[1] * (num7 - num9))) + (v[2] * (num6 + num10));
-    float num14 = ((v[0] * (num7 + num9)) + (v[1] * ((1.0f - num8) - num3))) + (v[2] * (num4 - num11));
-    float num13 = ((v[0] * (num6 - num10)) + (v[1] * (num4 + num11))) + (v[2] * ((1.0f - num8) - num5));
-    v[0] = num15;
-    v[1] = num14;
-    v[2] = num13;
-}
+//#define _USE_MATH_DEFINES // For M_PI_4
+//#include <math.h>
+//#include <cmath>
+//
+//Quaternion::Quaternion(float X, float Y, float Z, float W)
+//
+//
+//
+//
+//void Quaternion::RotateVector(std::vector<float>& v)
+//{
+//    std::vector<float> vector;
+//    float num12 = x + x;
+//    float num2 = y + y;
+//    float num = z + z;
+//    float num11 = w * num12;
+//    float num10 = w * num2;
+//    float num9 = w * num;
+//    float num8 = x * num12;
+//    float num7 = x * num2;
+//    float num6 = x * num;
+//    float num5 = y * num2;
+//    float num4 = y * num;
+//    float num3 = z * num;
+//    float num15 = ((v[0] * ((1.0f - num5) - num3)) + (v[1] * (num7 - num9))) + (v[2] * (num6 + num10));
+//    float num14 = ((v[0] * (num7 + num9)) + (v[1] * ((1.0f - num8) - num3))) + (v[2] * (num4 - num11));
+//    float num13 = ((v[0] * (num6 - num10)) + (v[1] * (num4 + num11))) + (v[2] * ((1.0f - num8) - num5));
+//    v[0] = num15;
+//    v[1] = num14;
+//    v[2] = num13;
+//}
--- Source/Quaternion.h
+++ Source/Quaternion.h
@@ -1,20 +1,75 @@
 #pragma once
-#include <vector>
+#include <array>
+#include <cmath>
+#include <numbers>
 
 struct Quaternion {
 	float x;
 	float y;
     float z;
 	float w;
-    Quaternion(float yaw, float pitch, float roll);
-    Quaternion(float x, float y, float z, float w);
-    Quaternion() { Quaternion(0, 0, 0); }
+
+    /*constexpr*/ Quaternion(float yaw, float pitch, float roll) {
+        // STD trigonometric functions not constexpr until 2026
+        pitch *= std::numbers::pi / 180.0f;
+        yaw *= std::numbers::pi / 180.0f;
+        roll *= std::numbers::pi / 180.0f;
+        x = std::sin(roll / 2) * std::cos(pitch / 2) * std::cos(yaw / 2) - std::cos(roll / 2) * std::sin(pitch / 2) * std::sin(yaw / 2);
+        y = std::cos(roll / 2) * std::sin(pitch / 2) * std::cos(yaw / 2) + std::sin(roll / 2) * std::cos(pitch / 2) * std::sin(yaw / 2);
+        z = std::cos(roll / 2) * std::cos(pitch / 2) * std::sin(yaw / 2) - std::sin(roll / 2) * std::sin(pitch / 2) * std::cos(yaw / 2);
+        w = std::cos(roll / 2) * std::cos(pitch / 2) * std::cos(yaw / 2) + std::sin(roll / 2) * std::sin(pitch / 2) * std::sin(yaw / 2);
+    };
+    constexpr Quaternion(float x, float y, float z, float w) :
+        x{x}, y{y}, z{z}, w{w}
+        { /*No extra processing*/ };
+
+    constexpr Quaternion() : Quaternion(0, 0, 0, 0)
+    {}
 
     // Basic operations
-    double Length() const;
-    Quaternion Normalize();
+    /*constexpr*/ [[nodiscard]] double Length() const {
+        // std::sqrt not constexpr until 2026
+        return std::sqrt(w * w + x * x + y * y + z * z);
+    };
+    /*constexpr*/ Quaternion Normalize() {
+        // std::sqrt not constexpr until 2026
+        float length = (float)Length();
+        w /= length;
+        x /= length;
+        y /= length;
+        z /= length;
+        return *this;
+    };
 
     // Complex operations
-    Quaternion Mul(const Quaternion& other) const;
-    void RotateVector(std::vector<float>& vector);
+    constexpr [[nodiscard]] Quaternion Mul(const Quaternion& other) const {
+        return Quaternion{
+            w * other.w - x * other.x - y * other.y - z * other.z,
+            w * other.x + x * other.w + y * other.z - z * other.y,
+            w * other.y - x * other.z + y * other.w + z * other.x,
+            w * other.z + x * other.y - y * other.x + z * other.w
+        };
+    };
+    constexpr [[nodiscard]] std::array<float, 3> Rotate3dArray(std::array<float, 3> array) const {
+
+        float num12 = x + x;
+        float num2 = y + y;
+        float num = z + z;
+        float num11 = w * num12;
+        float num10 = w * num2;
+        float num9 = w * num;
+        float num8 = x * num12;
+        float num7 = x * num2;
+        float num6 = x * num;
+        float num5 = y * num2;
+        float num4 = y * num;
+        float num3 = z * num;
+        float num15 = ((array[0] * ((1.0f - num5) - num3)) + (array[1] * (num7 - num9))) + (array[2] * (num6 + num10));
+        float num14 = ((array[0] * (num7 + num9)) + (array[1] * ((1.0f - num8) - num3))) + (array[2] * (num4 - num11));
+        float num13 = ((array[0] * (num6 - num10)) + (array[1] * (num4 + num11))) + (array[2] * ((1.0f - num8) - num5));
+        array[0] = num15;
+        array[1] = num14;
+        array[2] = num13;
+        return array;
+    };
 };
